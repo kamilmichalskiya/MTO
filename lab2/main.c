7c30d898eec86a644fa5d24d7c9a36cf006dda3e
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-10-09 12:43:17 +0200

    lab2 task finished

--- lab2/main.c
+++ lab2/main.c
@@ -33,33 +33,34 @@ int my_printf(char *format_string, char *param)
 		}
 		else if ((format_string[i] == '#') && (format_string[i + 1] == '.'))
 		{
-			int length = 0;
+			int number_of_chars = 0;
 			for (int j = i + 2; j < strlen(format_string); ++j)
 			{
 				if (j > i + 2 && format_string[j] == 'k')
 				{
-					length = j - i;
+					number_of_chars = j - (i + 2);
 				}
 				else if (!isdigit(format_string[j]))
 				{
 					break;
 				}
 			}
-			if (length == 0)
+			if (number_of_chars == 0)
 			{
 				putchar(format_string[i]);
 			}
 			else
 			{
-				char number[length + 1];
-				memcpy(number, &format_string[i + 2], length);
-				number[length] = '\0';
-				int num = atoi(number);
+				i += 2;
+				char number_in_chars[number_of_chars + 1];
+				memcpy(number_in_chars, &format_string[i], number_of_chars);
+				number_in_chars[number_of_chars] = '\0';
+				int num = atoi(number_in_chars);
 				for (int j = 0; j < strlen(param) && j < num; j++)
 				{
 					putchar(swap_case_sesnsitivity(param[j]));
 				}
-				i += length;
+				i += number_of_chars;
 			}
 		}
 		else
