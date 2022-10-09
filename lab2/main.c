6f43a20c8e73e8f8fcc4146a5f2bf269a882e7d9
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-10-09 11:20:07 +0200

    Add another if for lab2

--- lab2/main.c
+++ lab2/main.c
@@ -2,30 +2,63 @@
 #include <string.h>
 #include <ctype.h>
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+int my_printf(char *format_string, char *param)
+{
+	for (int i = 0; i < strlen(format_string); i++)
+	{
+		if ((format_string[i] == '#') && (format_string[i + 1] == 'k'))
+		{
 			i++;
-			for(int j=0;j<strlen(param);j++){
+			for (int j = 0; j < strlen(param); j++)
+			{
 				char value = param[j];
-				if(islower(value)){
+				if (islower(value))
+				{
 					value = toupper(value);
-				}else if(isupper(value)){
+				}
+				else if (isupper(value))
+				{
 					value = tolower(value);
 				}
 				putchar(value);
 			}
-		}else
+		}
+		else if ((format_string[i] == '#') && (format_string[i + 1] == '.'))
+		{
+			int length = 0;
+			for (int j = i; j < strlen(format_string); ++j)
+			{
+				if (format_string[j] == 'k')
+				{
+					length = j;
+				}
+				else if (!isdigit(format_string[j]))
+				{
+					break;
+				}
+			}
+			if (length == 0)
+			{
+				putchar(format_string[i]);
+			}
+			else
+			{
+				// wyświetl określoną liczbę znaków
+			}
+		}
+		else
 			putchar(format_string[i]);
 	}
 	puts("");
 }
 
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
+int main(int argc, char *argv[])
+{
+	char buf[1024], buf2[1024];
+	while (gets(buf))
+	{
 		gets(buf2);
-		my_printf(buf,buf2);
+		my_printf(buf, buf2);
 	}
 	return 0;
 }
