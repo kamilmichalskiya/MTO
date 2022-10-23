e4c66bdac9a9a24b4b1164951abb9689bf0b4087
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-10-23 11:19:53 +0200

    Lab3 add implementation

--- lab3/main.c
+++ lab3/main.c
@@ -63,6 +63,43 @@ int my_printf(char *format_string, char *param)
 				i += number_of_chars;
 			}
 		}
+		else if (format_string[i] == '#')
+		{
+			int number_of_chars = 0;
+			for (int j = i + 1; j < strlen(format_string); ++j)
+			{
+				if (j > i + 1 && format_string[j] == 'k')
+				{
+					number_of_chars = j - (i + 1);
+				}
+				else if (!isdigit(format_string[j]))
+				{
+					break;
+				}
+			}
+			if (number_of_chars == 0)
+			{
+				putchar(format_string[i]);
+			}
+			else
+			{
+				i += 1;
+				char number_in_chars[number_of_chars + 1];
+				memcpy(number_in_chars, &format_string[i], number_of_chars);
+				number_in_chars[number_of_chars] = '\0';
+				int num = atoi(number_in_chars);
+				for (int j = 0; j < strlen(param); j++)
+				{
+					putchar(swap_case_sesnsitivity(param[j]));
+				}
+				for (int j = strlen(param); j < num; j++)
+				{
+					putchar(' ');
+				}
+				
+				i += number_of_chars;
+			}
+		}
 		else
 			putchar(format_string[i]);
 	}
