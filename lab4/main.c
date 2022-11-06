d2a5434ae75646ba69f17767613bfc9c45cdcfd2
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-11-06 11:25:11 +0100

    Lab4 Skip no number

--- lab4/main.c
+++ lab4/main.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
+int my_printf(char *format_string, char *param)
+{
+	for (int i = 0; i < strlen(format_string); i++)
+	{
+		if ((format_string[i] == '#') && (format_string[i + 1] == 'g'))
+		{
 			i++;
-			printf("%d",param);
-		}else
+			int len = 0;
+			for (int j = 0; j < strlen(param); j++)
+			{
+				if (!isdigit(param[j]))
+				{
+					len = j;
+					break;
+				}
+			}
+			
+			if (len)
+			{
+				puts("");
+				return -1;
+			}
+			else
+			{
+				printf("%d", atoi(param));
+			}
+		}
+		else
 			putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
 }
 
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
+int main(int argc, char *argv[])
+{
+	char buf[1024], buf2[1024];
+	while (gets(buf))
+	{
 		gets(buf2);
-		my_printf(buf,buf2);
+		my_printf(buf, buf2);
 	}
 	return 0;
 }
