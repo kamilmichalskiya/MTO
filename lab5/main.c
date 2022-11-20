ca12721adb3101ac70b543bd9393308bfc3f010f
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-11-20 11:10:32 +0100

    Lab5 implementation

--- lab5/main.c
+++ lab5/main.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int my_printf(char *format_string, char *param)
 {
 	for (int i = 0; i < strlen(format_string); i++)
 	{
-		if ((format_string[i] == '#') && (format_string[i + 1] == 'k'))
+		if ((format_string[i] == '#') && (format_string[i + 1] == 'X') && (format_string[i + 2] == 'g'))
 		{
-			i++;
-			printf("%s", param);
+			i += 2;
+			int j;
+			for (j = 0; j < strlen(param); j++)
+			{
+				char c = param[j];
+				if (!isdigit(c))
+				{
+					break;
+				}
+				else
+				{
+					switch (c)
+					{
+					case '0':
+						param[j] = '9';
+						break;
+					case '1':
+						param[j] = '0';
+						break;
+					case '2':
+						param[j] = '1';
+						break;
+					case '3':
+						param[j] = '2';
+						break;
+					case '4':
+						param[j] = '3';
+						break;
+					case '5':
+						param[j] = '4';
+						break;
+					case '6':
+						param[j] = '5';
+						break;
+					case '7':
+						param[j] = '6';
+						break;
+					case '8':
+						param[j] = '7';
+						break;
+					case '9':
+						param[j] = '8';
+						break;
+					default:
+						break;
+					}
+				}
+			}
+
+			if (j < strlen(param))
+			{
+				puts("");
+				return -1;
+			}
+			else
+			{
+				printf("%s", param);
+			}
 		}
 		else
 			putchar(format_string[i]);
