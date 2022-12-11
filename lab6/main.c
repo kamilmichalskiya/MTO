15bb6437016bd5489ef88bae2cb7eb4ffda45325
kamil.michalski.ya@gmail.com <kamil.michalski.ya@gmail.com>
2022-12-11 11:08:13 +0100

    Lab6 Reformat code

--- lab6/main.c
+++ lab6/main.c
@@ -3,9 +3,12 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == '.')){
+int my_printf(char *format_string, char *param)
+{
+	for (int i = 0; i < strlen(format_string); i++)
+	{
+		if ((format_string[i] == '#') && (format_string[i + 1] == '.'))
+		{
 			int number_of_chars = 0;
 			for (int j = i + 2; j < strlen(format_string); ++j)
 			{
@@ -35,17 +38,20 @@ int my_printf(char *format_string, char *param){
 				}
 				i += number_of_chars;
 			}
-		}else
+		}
+		else
 			putchar(format_string[i]);
 	}
 	puts("");
 }
 
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
+int main(int argc, char *argv[])
+{
+	char buf[1024], buf2[1024];
+	while (gets(buf))
+	{
 		gets(buf2);
-		my_printf(buf,buf2);
+		my_printf(buf, buf2);
 	}
 	return 0;
 }
